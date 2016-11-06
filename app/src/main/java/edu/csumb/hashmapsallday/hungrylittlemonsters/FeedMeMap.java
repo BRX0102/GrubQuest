832c0f6c5e18a5d8d5ce01f86c6d1e1301d146c3
brandanlockwood <blockwood@csumb.edu>
2016-11-05 23:37:11 -0700

    FeedMe

---
+++ app/src/main/java/edu/csumb/hashmapsallday/hungrylittlemonsters/FeedMeMap.java
@@ -0,0 +1,21 @@
+package edu.csumb.hashmapsallday.hungrylittlemonsters;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by brand on 11/5/2016.
+ */
+
+public class FeedMeMap extends AppCompatActivity {
+    private TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_back);
+
+    }
+
+}
--- app/src/main/java/edu/csumb/hashmapsallday/hungrylittlemonsters/MainActivity.java
+++ app/src/main/java/edu/csumb/hashmapsallday/hungrylittlemonsters/MainActivity.java
@@ -1,13 +1,21 @@
 package edu.csumb.hashmapsallday.hungrylittlemonsters;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+   private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent myIntent = new Intent(this, FeedMe.class);
+        startActivity(myIntent);
     }
+
 }
