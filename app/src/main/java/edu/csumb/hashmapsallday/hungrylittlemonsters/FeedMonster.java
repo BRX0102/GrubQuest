01f8146ea786f5dee14dde1c05e8aac597e99f42
Ernesto Cortez <Ernesto Cortez>
2016-11-05 22:24:58 -0700

    Add monster face and wave animation to FeedMonster

--- app/src/main/java/edu/csumb/hashmapsallday/hungrylittlemonsters/FeedMonster.java
+++ app/src/main/java/edu/csumb/hashmapsallday/hungrylittlemonsters/FeedMonster.java
@@ -4,6 +4,7 @@ package edu.csumb.hashmapsallday.hungrylittlemonsters;
 import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -40,22 +42,15 @@ public class FeedMonster extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedmonster);
-        column1 = (View)findViewById(R.id.item1Column);
-        column2 = (View)findViewById(R.id.item2Column);
-        column3 = (View)findViewById(R.id.item3Column);
-        monster = ((ImageView)findViewById(R.id.monster));
 
-        startIdleAnimation(monster);
-
+        resetActivity(null);
+        monsterDefaultFace(findViewById(R.id.monsterColumn));
 //        column1.setOnDragListener(new DragListener());
 //        ((ViewGroup)column1).removeAllViews();
 //        FoodDragItem temp = new FoodDragItem(this);
 //        temp.setImageResource(R.drawable.broccoli);
 //        ((ViewGroup)column1).addView(temp);
-        setItemView(column1, new FoodDragItem(this), R.drawable.broccoli);
-        setItemView(column2, new FoodDragItem(this), R.drawable.coins);
-        setItemView(column3, new FoodDragItem(this), R.drawable.utensils);
-        monster.setOnDragListener(new DragListener());
+
 
     }
 
@@ -67,23 +62,21 @@ public class FeedMonster extends AppCompatActivity {
             int action = event.getAction();
             switch (action) {
                 case DragEvent.ACTION_DRAG_STARTED:
-                    v.setBackgroundColor(LIGHT_BLUE);
+                    startIdleAnimation(monster);
                     v.invalidate();
                     return true;
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    v.setBackgroundColor(LIGHT_GREEN);
                     v.invalidate();
                     return true;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    v.setBackgroundColor(LIGHT_BLUE);
                     v.invalidate();
                     return true;
                 case DragEvent.ACTION_DRAG_ENDED:
-                    v.setBackgroundColor(Color.WHITE);
                     v.invalidate();
                     return true;
                 case DragEvent.ACTION_DROP:
                     counter++;
+                    startWaveAnimation(monster);
 
                     // Dropped foodTile on monster
                     FoodDragItem temp = (FoodDragItem) event.getLocalState();
@@ -116,11 +109,6 @@ public class FeedMonster extends AppCompatActivity {
         ((ViewGroup)column).addView(item);
     }
 
-    private void passAnimationBackground(){
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
     private void startIdleAnimation(ImageView monster){
 
 
@@ -131,12 +119,39 @@ public class FeedMonster extends AppCompatActivity {
         frameAnimation.start();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void animateEatingObject(ImageView monster){
+    private void startWaveAnimation(ImageView monster){
+        monster.setImageResource(R.drawable.wave_monster);
+
+        AnimationDrawable frameAnimation = (AnimationDrawable)monster.getDrawable();
+
+        frameAnimation.start();
+    }
+
+    private void monsterDefaultFace(View v){
+
+        ImageView eyes = new ImageView(this);
+        eyes.setImageResource(R.drawable.crosseye);
+        ((RelativeLayout)v).addView(eyes);
+
+        ImageView mouth = new ImageView(this);
+        mouth.setImageResource(R.drawable.grinsmile);
+        ((RelativeLayout)v).addView(mouth);
+
+    }
 
 
+    public void resetActivity(View v){
+        column1 = (View)findViewById(R.id.item1Column);
+        column2 = (View)findViewById(R.id.item2Column);
+        column3 = (View)findViewById(R.id.item3Column);
+        monster = ((ImageView)findViewById(R.id.monster));
 
         startIdleAnimation(monster);
+
+        setItemView(column1, new FoodDragItem(this), R.drawable.broccoli);
+        setItemView(column2, new FoodDragItem(this), R.drawable.coins);
+        setItemView(column3, new FoodDragItem(this), R.drawable.utensils);
+        monster.setOnDragListener(new DragListener());
     }
 
     private void submitMonsterPreferences(){
