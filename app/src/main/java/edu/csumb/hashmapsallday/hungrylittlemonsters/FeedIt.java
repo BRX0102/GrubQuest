9c6f4254d1fb731ab66d776ab86040500edc910b
Ernesto Cortez <Ernesto Cortez>
2016-11-06 10:23:19 -0800

    More changes

--- app/src/main/java/edu/csumb/hashmapsallday/hungrylittlemonsters/FeedIt.java
+++ app/src/main/java/edu/csumb/hashmapsallday/hungrylittlemonsters/FeedIt.java
@@ -159,42 +159,7 @@ public class FeedIt extends AppCompatActivity {
 
     }
 
-    private void monsterSetFace(View v){
 
-        int selector;
-
-        selector = new Random().nextInt(3);
-
-        if (selector == 0) {
-            monsterSetFaceHelper(v, R.drawable.crosseye, 1);
-        } else if (selector == 1) {
-            monsterSetFaceHelper(v, R.drawable.closereyes, 1);
-        } else if (selector == 2) {
-            monsterSetFaceHelper(v, R.drawable.crazyeye, 1);
-        }
-
-        selector = new Random().nextInt(3);
-
-        if (selector ==0) {
-            monsterSetFaceHelper(v, R.drawable.grinsmile, 2);
-        } else if (selector ==1) {
-            monsterSetFaceHelper(v, R.drawable.smile, 2);
-        } else if (selector ==2) {
-            monsterSetFaceHelper(v, R.drawable.licksmile, 2);
-        }
-
-
-
-//        if (selector ==0) {
-//            monsterSetFaceHelper(v, R.drawable.horntail, 3);
-//        } else if (selector ==1) {
-//            monsterSetFaceHelper(v, R.drawable.horns, 3);
-//        } else if (selector ==2) {
-//            monsterSetFaceHelper(v, R.drawable.batwing, 3);
-//        }
-
-
-    }
 
     private void monsterSetFaceHelper(View v, int drawableID, int selector){
 
@@ -214,13 +179,11 @@ public class FeedIt extends AppCompatActivity {
 
     public void resetActivity(View v){
         column1 = (View)findViewById(R.id.item1Column);
-        column2 = (View)findViewById(R.id.item2Column);
-        column3 = (View)findViewById(R.id.item3Column);
         monster = ((ImageView)findViewById(R.id.monster));
 
         startIdleAnimation(monster);
 
-        setItemView(column1, new FoodDragItem(this), R.drawable.burger);
+        setItemView(column1, new FoodDragItem(this), R.drawable.burger_bigger);
         monster.setOnDragListener(new DragListener());
         monsterDefaultFace(findViewById(R.id.monsterColumn));
         counter = 0;
